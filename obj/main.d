03193ca0bda442668c0eabd05ec97041d6ffbdb1
Anna Gawrysiak <70595292+AnnaGawrysiak@users.noreply.github.com>
2022-10-09 20:42:23 +0200

    Delete obj directory

--- obj/main.d
+++
@@ -1,6 +0,0 @@
-obj/main.o: src/main.cpp src/Greeter.hpp src/Updater.hpp \
- src/GreeterUpdater.hpp src/LazyUpdater.hpp
-src/Greeter.hpp:
-src/Updater.hpp:
-src/GreeterUpdater.hpp:
-src/LazyUpdater.hpp:
